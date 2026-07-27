ab659a7814846064669d4be9f3e68c211351e3d9
Nicholas Adamou <10106289+nicholasadamou@users.noreply.github.com>
2026-07-27 15:12:59 -0700

    feat: load prompt profiles from adapters

--- integrations/starship.bash
+++ integrations/starship.bash
@@ -4,9 +4,15 @@
 # see: https://starship.rs
 
 SMU_PROMPT="${SMU_PROMPT:-starship}"
+prompt_adapter="${BASH_CONFIG_DIR:-$HOME/.config/bash}/prompts/${SMU_PROMPT}.bash"
 
-if [ "$SMU_PROMPT" = "classic" ]; then
-    PS1='\u@\h:\w\$ '
+if [ -r "$prompt_adapter" ]; then
+    # shellcheck source=/dev/null
+    . "$prompt_adapter"
 elif command -v starship &>/dev/null; then
     eval "$(starship init bash)"
+else
+    PS1='\u@\h:\w\$ '
 fi
+
+unset prompt_adapter
